e7ab3723c7206a89d671f25f7e5c14d577909d77
Tagite <daki403.1@gmail.com>
2025-01-18 16:45:51 +0900

    ♻️ refactor(entites/): remove unused entites
    entites를 경량화하기 위해서 쓸모 없는 것들을 삭제 헀습니다.

--- entites/label.py
+++ entites/label.py
@@ -1,19 +1,52 @@
 from __future__ import annotations
-from pydantic import BaseModel, Field
+from typing_extensions import Self
+
 import uuid
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import field_validator
+from pydantic import model_validator
+from pydantic import ValidationError
 
 
 class Pos(BaseModel):
     x: int
     y: int
-    
+
+    @field_validator("x", "y", mode="after")
+    @classmethod
+    def ensure_positive(cls, value: int) -> int:
+        if value < 0:
+            raise ValidationError(f"{value} is not an positive number")
+        return value
+
     class Config:
         frozen = True
-
+    
 
 class BBox(BaseModel):
     top_left: Pos
     bottom_right: Pos
+
+    @model_validator(mode="after")
+    def check_relative_pos(self) -> Self:
+        if self.top_left.x > self.bottom_right.x \
+                or self.top_left.y < self.bottom_right.y:
+            raise ValueError("top_left & bottom_right relative don't match")
+
+    class Config:
+        frozen = True
+
+
+class LabelType(BaseModel):
+    name: str
+
+    @field_validator("name", mode="after")
+    @classmethod
+    def ensure_not_blank(cls, value: str) -> str:
+        if not len(value.strip()):
+            raise ValidationError('name is blank')
+            
 
     class Config:
         frozen = True
@@ -22,9 +55,9 @@ class BBox(BaseModel):
 class Label(BaseModel):
     id: uuid.UUID = Field(
                         default_factory=uuid.uuid4,
-                        private=True)
+                        )
     bbox: BBox
-    label_type: str
+    label_type: LabelType
 
     class Config:
         frozen = True
